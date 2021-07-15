ea5d06536087a6c21a23e1ab6dba91189317cdd3
mohammed97-eldor <62855600+mohammed97-eldor@users.noreply.github.com>
2021-07-15 11:02:48 +0300

    Add files via upload

---
+++ Visualization.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import math
+import numpy as np
+from collections import OrderedDict
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import auc
+
+def ams2(s, b):
+    br = 10
+    arg = 2 * ((s + b + br) * math.log(1 + s/(b + br)) - s)
+    return math.sqrt(arg)
+
+
+def ams3(s, b):
+    br = 10
+    return s / math.sqrt(b + br)
+
+def create_result_df(scores, labels, weights):
+    
+    df = pd.DataFrame({
+        'score': scores,
+        'label': labels,
+        'weight': weights
+    })
+
+    assert scores.index.equals(labels.index)
+    assert scores.index.equals(weights.index)
+
+    mn = min(0.0, np.percentile(scores, 2))
+    mx = min(1.0, np.percentile(scores, 98))
+
+    delta = mx - mn
+    thresholds = np.arange(mn, mx, delta/100)
+
+    res = []
+
+    for t in thresholds:
+
+        d = df[df.score >= t]
+
+        r = OrderedDict()
+        r['threshold'] = t
+        r['count'] = len(d)
+        r['num_sig'] = len(d[d.label == 's'])
+        r['num_bkg'] = len(d[d.label == 'b'])
+        r['weight'] = d.weight.sum()
+
+        r['s'] = d[d.label=='s'].weight.sum()
+        r['b'] = d[d.label=='b'].weight.sum()
+
+        r['ams2'] = ams2(r['s'], r['b'])
+        r['ams3'] = ams3(r['s'], r['b'])
+
+        n_tp = len(df[(df.score >= t) & (df.label == 's')])
+        n_tn = len(df[(df.score < t) & (df.label == 'b')])
+
+        w_tp = df[(df.score >= t) & (df.label == 's')].weight.sum()
+        w_tn = df[(df.score < t) & (df.label == 'b')].weight.sum()
+
+        n_fn = len(df[df.score < t]) - n_tn
+        w_fn = df[df.score < t].weight.sum() - w_tn
+
+        n_fp = len(d) - n_tp
+        w_fp = d.weight.sum() - w_tp
+
+        r['accuracy'] = (n_tp + n_tn) / len(df)
+        r['accuracy_wgt'] = (w_tp + w_tn) / df.weight.sum()
+
+        r['true_positive_rate'] = n_tp / (n_tp + n_fn)
+        r['true_positive_rate_wgt'] = w_tp / (w_tp + w_fn)
+
+        r['false_positive_rate'] = n_fp / (n_fp + n_tn)
+        r['false_positive_rate_wgt'] = w_fp / (w_fp + w_tn)
+
+        res.append(r)
+
+    return df, pd.DataFrame(res).set_index('threshold', drop=False)
+
+
+# Helper Function for Plotting
+
+def plot_results(df_scores, df_summary):
+
+    fig = plt.figure(figsize=(12,12))
+
+    plt.subplot(3, 2, 1)
+    for l in ['b', 's']:
+        sns.distplot(
+            df_scores[df_scores.label == l].score,
+            label=l,
+            ax=plt.gca(),
+            hist=False,
+            rug=False)
+    plt.legend(loc='best')
+    plt.title("Individual distributions of score within each class")
+
+    plt.subplot(3, 2, 2)
+    df_summary.b.plot(label='b')
+    df_summary.s.plot(label='s')
+    plt.legend(loc='best')
+    plt.title("s and b as a function of threshold")
+    plt.gca().set_yscale("log", nonposy='clip')
+
+    plt.subplot(3, 2, 3)
+    mn, mx = df_summary.threshold.min(), df_summary.threshold.max()
+    plt.hist(df_scores[df_scores.label=='b'].score,
+             weights=df_scores[df_scores.label=='b'].weight,
+             bins=np.arange(mn, mx, (mx-mn) / 100),
+             label='b',
+             stacked=True)
+    plt.hist(df_scores[df_scores.label=='s'].score,
+             weights=df_scores[df_scores.label=='s'].weight,
+             bins=np.arange(mn, mx, (mx-mn) / 100),
+             label='s',
+             stacked=True)
+    plt.gca().set_yscale("log", nonposy='clip')
+    plt.legend(loc='best')
+    plt.title("Weighted histograms of score distributions (stacked)")
+    plt.xlabel('score')
+
+    plt.subplot(3, 2, 4)
+    df_summary.ams2.plot(label='ams2 (max={:.2f})'.format(df_summary.ams2.max()))
+    df_summary.ams3.plot(label='ams3 (max={:.2f})'.format(df_summary.ams3.max()))
+    plt.xlabel('Threshold')
+    plt.ylabel('AMS Metric')
+    plt.legend(loc='best')
+    plt.title("Metrics as a function of threshold")
+
+    plt.subplot(3, 2, 5)
+    x = df_summary['false_positive_rate']
+    y = df_summary['true_positive_rate']
+    roc_auc = auc(x, y)
+    plt.plot(x, y, label='ROC Curve     (AUC={:.3f})'.format(roc_auc))
+    x = df_summary['false_positive_rate_wgt']
+    print(x.shape,type(x))
+    for i in range(0,len(x)-1):
+      if x.iloc[i] == x.iloc[i+1]:
+        #print(i)
+        x.iloc[i+1] = x.iloc[i+1] - 0.0000001
+      if x.iloc[i] < x.iloc[i+1]:
+        x.iloc[i+1] = x.iloc[i] - 0.0000001
+    y = df_summary['true_positive_rate_wgt']
+    roc_auc = auc(x, y)
+    plt.plot(x, y, label='ROC Curve WGT (AUC={:.3f})'.format(roc_auc))
+    plt.xlabel('False Positive Rate')
+    plt.ylabel("True Positive Rate") 
+    plt.xlim([0, 1])
+    plt.ylim([0, 1])
+    plt.legend(loc='best')
+
+    plt.subplot(3, 2, 6)
+    df_summary.accuracy.plot(label='Accuracy') # (max={:.3f})'.format(df_summary.accuracy.max()))
+    df_summary.accuracy_wgt.plot(label='Accuracy WGT') # (max={:.3f})'.format(df_summary.accuracy_wgt.max()))
+    plt.legend(loc='best')
+
+    plt.tight_layout()
---
+++ features.py
@@ -0,0 +1,365 @@
+
+from functools import wraps
+
+import pandas
+
+import numpy as np
+
+import pandas as pd
+
+# Helpers
+
+def px(pt, eta, phi):
+    return np.where(pt >= 0, pt * np.cos(phi), -999)
+
+def py(pt, eta, phi):
+    return np.where(pt >= 0, pt * np.sin(phi), -999)
+
+def pz(pt, eta, phi):
+    return np.where(pt >= 0, pt * np.sinh(eta), -999)
+
+def p_tot(pt, eta, phi):
+    x = px(pt, eta, phi)
+    y = py(pt, eta, phi)
+    z = pz(pt, eta, phi)
+    return np.sqrt(x*x + y*y + z*z)
+
+def _calculate_momenta(df, prefix):
+    pt = df[prefix+'pt']
+    eta = df[prefix+'eta']
+    phi = df[prefix+'phi']
+
+    return pd.DataFrame({
+        prefix+'px': px(pt, eta, phi),
+        prefix+'py': py(pt, eta, phi),
+        prefix+'pz': pz(pt, eta, phi),
+        prefix+'p_tot': p_tot(pt, eta, phi)
+    }, index=df.index).fillna(-999)
+
+
+def total_maximum(xs):
+    m = None
+    for x in xs:
+        if m is None:
+            m = x
+        else:
+            m = np.maximum(m, x)
+    return m
+
+
+def total_minimum(xs):
+    m = None
+    for x in xs:
+        if m is None:
+            m = x
+        else:
+            m = np.minimum(m, x)
+    return m
+
+
+def require_jets(njets, default=-999):
+    def feature_decorator(func):
+        @wraps(func)
+        def func_wrapper(df):
+            return np.where(df.PRI_jet_num >= njets, func(df), default)
+        return func_wrapper
+    return feature_decorator
+
+
+# The distance from the x=y line in jet eta0, eta1 space
+def eta_plus(x, y):
+    return np.sqrt(x*x/2 + 2*y*y - 2*x*y)
+
+
+# Create momentum variables
+
+def with_momentum_features(df):
+
+    df = df.copy()
+
+    lep = _calculate_momenta(df, 'PRI_lep_')
+    jet_leading = _calculate_momenta(df, 'PRI_jet_leading_')
+    jet_subleading = _calculate_momenta(df, 'PRI_jet_subleading_')
+    tau = _calculate_momenta(df, 'PRI_tau_')
+
+    for df_mom in [lep, jet_leading, jet_subleading, tau]:
+        for col, srs in df_mom.iteritems():
+           df[col] = srs
+
+    return df
+
+#
+# Start creating features
+#
+
+#
+# Eta Features
+#
+
+@require_jets(2)
+def jet_eta_plus(df):
+    x = df['PRI_jet_leading_eta']
+    y = df['PRI_jet_subleading_eta']
+    return np.where((x > -900) & (y > -900),
+                    eta_plus(x, y),
+                    -999)
+
+# Do the same with the lepton and jet
+def lep_tau_eta_plus(df):
+    x = df['PRI_lep_eta']
+    y = df['PRI_tau_eta']
+    return eta_plus(x, y)
+
+rapidity_features = [jet_eta_plus, lep_tau_eta_plus]
+
+#
+# Z Momentum Features
+#
+
+def lep_z_momentum(df):
+    return df['PRI_lep_pz'] + df['PRI_tau_pz']
+
+@require_jets(2)
+def jet_z_momentum(df):
+    return df['PRI_jet_leading_pz'] + df['PRI_jet_subleading_pz']
+
+@require_jets(2)
+def jet_lep_sum_z_momentum(df):
+    return lep_z_momentum(df) + jet_z_momentum(df)
+
+@require_jets(2)
+def jet_lep_diff_z_momentum(df):
+    return lep_z_momentum(df) - jet_z_momentum(df)
+
+z_momentum_features = [lep_z_momentum, jet_z_momentum, jet_lep_sum_z_momentum, jet_lep_diff_z_momentum]
+
+#
+# Transverse Momenta Features
+#
+
+def max_jet_pt(df):
+    return np.maximum(
+        np.where(df['PRI_jet_leading_pt'] > 0, df['PRI_jet_leading_pt'], 0),
+        np.where(df['PRI_jet_subleading_pt'] > 0, df['PRI_jet_subleading_pt'], 0))
+
+def min_jet_pt(df):
+    return np.minimum(
+        np.where(df['PRI_jet_leading_pt'] > 0, df['PRI_jet_leading_pt'], 0),
+        np.where(df['PRI_jet_subleading_pt'] > 0, df['PRI_jet_subleading_pt'], 0))
+
+def max_lep_pt(df):
+    return np.maximum(df['PRI_tau_pt'], df['PRI_lep_pt'])
+
+def min_lep_pt(df):
+    return np.minimum(df['PRI_tau_pt'], df['PRI_lep_pt'])
+
+def max_pt(df):
+    return np.maximum(max_jet_pt(df), max_lep_pt(df))
+
+def min_pt(df):
+    return np.minimum(min_jet_pt(df), min_lep_pt(df))
+
+def sum_jet_pt(df):
+    return np.where(df['PRI_jet_leading_pt'] > 0, df['PRI_jet_leading_pt'], 0) + np.where(df['PRI_jet_subleading_pt']>0, df['PRI_jet_subleading_pt'], 0)
+
+def sum_lep_pt(df):
+    return df['PRI_tau_pt'] + df['PRI_lep_pt']
+
+
+transverse_momentum_features = [max_jet_pt, min_jet_pt, max_lep_pt, min_lep_pt,
+                                max_pt, min_pt, sum_jet_pt, sum_lep_pt]
+
+#
+# Momentum Ratio Features
+#
+
+def frac_tau_pt(df):
+    tau_pt = df['PRI_tau_pt']
+    lep_pt = df['PRI_lep_pt']
+    pt_sum = (tau_pt + lep_pt)
+    return np.where(pt_sum > 0, tau_pt / pt_sum, -999)
+
+def frac_lep_pt(df):
+    tau_pt = df['PRI_tau_pt']
+    lep_pt = df['PRI_lep_pt']
+    pt_sum = (tau_pt + lep_pt)
+    return np.where(pt_sum > 0, lep_pt / pt_sum, -999)
+
+def frac_tau_p(df):
+    tau_p = df['PRI_tau_p_tot']
+    lep_p = df['PRI_lep_p_tot']
+    p_sum = (tau_p + lep_p)
+    return np.where(p_sum > 0, tau_p / p_sum, -999)
+
+def frac_lep_p(df):
+    tau_p = df['PRI_tau_p_tot']
+    lep_p = df['PRI_lep_p_tot']
+    p_sum = (tau_p + lep_p)
+    return np.where(p_sum != 0, lep_p / p_sum, -999)
+
+
+@require_jets(2)
+def prijet_subjet_pt_ratio(df):
+    return df['PRI_jet_leading_pt'] / (df['PRI_jet_leading_pt'] + df['PRI_jet_subleading_pt'])
+
+@require_jets(2)
+def subjet_prijet_pt_ratio(df):
+    return df['PRI_jet_subleading_pt'] / (df['PRI_jet_leading_pt'] + df['PRI_jet_subleading_pt'])
+
+momentum_ratio_features = [frac_tau_pt, frac_lep_pt, frac_tau_p, frac_lep_p,
+                           prijet_subjet_pt_ratio, subjet_prijet_pt_ratio]
+
+
+
+#
+# Mass / Energy Features
+#
+
+
+def transverse_mass_lep(row):
+    lep_pt = np.sqrt(row['PRI_lep_px']*row['PRI_lep_px'] + row['PRI_lep_py']*row['PRI_lep_py'])
+    return np.sqrt(2*lep_pt*row['PRI_met'] * (1 - (np.cos(row['PRI_met_phi'] - row['PRI_lep_phi']))))
+
+def transverse_mass_tau(row):
+    tau_pt = np.sqrt(row['PRI_tau_px']*row['PRI_tau_px'] + row['PRI_tau_py']*row['PRI_tau_py'])
+    return np.sqrt(2*tau_pt*row['PRI_met'] * (1 - np.cos(row['PRI_met_phi'] - row['PRI_tau_phi'])))
+
+def transverse_mass_jet_leading(row):
+    lep_pt = np.sqrt(row['PRI_jet_leading_px']*row['PRI_jet_leading_px'] + row['PRI_jet_leading_py']*row['PRI_jet_leading_py'])
+    return np.sqrt(2*lep_pt*row['PRI_met'] * (1 - (np.cos(row['PRI_met_phi'] - row['PRI_jet_leading_phi']))))
+
+def transverse_mass_jet_subleading(row):
+    lep_pt = np.sqrt(row['PRI_jet_subleading_px']*row['PRI_jet_subleading_px'] + row['PRI_jet_subleading_py']*row['PRI_jet_subleading_py'])
+    return np.sqrt(2*lep_pt*row['PRI_met'] * (1 - (np.cos(row['PRI_met_phi'] - row['PRI_jet_subleading_phi']))))
+
+
+tau_mass = 1.7
+
+def tau_fourenergy(row):
+    tau_2 = row['PRI_tau_px']*row['PRI_tau_px'] + row['PRI_tau_py']*row['PRI_tau_py'] + row['PRI_tau_pz']*row['PRI_tau_pz']
+    return np.sqrt(tau_mass*tau_mass + tau_2)
+
+def lep_fourenergy(row):
+    lep_2 = row['PRI_lep_px']*row['PRI_lep_px'] + row['PRI_lep_py']*row['PRI_lep_py'] + row['PRI_lep_pz']*row['PRI_lep_pz']
+    return np.sqrt(tau_mass*tau_mass + lep_2)
+
+
+def lep_mass(df):
+    delta_cosh_eta = np.cosh(df['PRI_lep_eta'] - df['PRI_tau_eta'])
+    delta_cos_phi = np.cos(df['PRI_lep_phi'] - df['PRI_tau_phi'])
+    return 2*df['PRI_lep_pt']*df['PRI_tau_pt']*(delta_cosh_eta - delta_cos_phi)
+
+
+
+mass_energy_features = [transverse_mass_lep, transverse_mass_tau, transverse_mass_jet_leading,
+                        transverse_mass_jet_subleading, tau_fourenergy, lep_fourenergy, lep_mass]
+
+#
+# MET Features
+#
+
+def ht(df):
+    return sum_jet_pt(df) + sum_lep_pt(df)
+
+def ht_met(df):
+    return ht(df) + df['PRI_met']
+
+def tau_met_cos_phi(df):
+    return np.cos(df['PRI_met_phi'] - df['PRI_tau_phi'])
+
+def lep_met_cos_phi(df):
+    return np.cos(df['PRI_met_phi'] - df['PRI_lep_phi'])
+
+@require_jets(1)
+def jet_leading_met_cos_phi(df):
+    return np.cos(df['PRI_jet_leading_phi'] - df['PRI_lep_phi'])
+
+@require_jets(2)
+def jet_subleading_met_cos_phi(df):
+    return np.cos(df['PRI_jet_subleading_phi'] - df['PRI_lep_phi'])
+
+@require_jets(2)
+def min_met_cos_phi(df):
+    return total_minimum([
+        tau_met_cos_phi(df),
+        lep_met_cos_phi(df),
+        jet_leading_met_cos_phi(df),
+        jet_subleading_met_cos_phi(df)])
+
+@require_jets(2)
+def max_met_cos_phi(df):
+    return total_maximum([
+        tau_met_cos_phi(df),
+        lep_met_cos_phi(df),
+        jet_leading_met_cos_phi(df),
+        jet_subleading_met_cos_phi(df)])
+
+def met_sig(df):
+    return np.where(df['PRI_met_sumet'] > 0,
+                    df['PRI_met'] / np.sqrt(df['PRI_met_sumet']),
+                    -999)
+
+def sumet_sum_pt_ratio(df):
+    return np.where(df['DER_sum_pt'] != 0,
+                    df['PRI_met_sumet'] / df['DER_sum_pt'],
+                    -999)
+
+def met_pt_total_ratio(df):
+    return np.where(df['DER_pt_tot']==0, -999, df['PRI_met'] / df['DER_pt_tot'])
+
+met_features = [ht, ht_met, tau_met_cos_phi, lep_met_cos_phi, jet_leading_met_cos_phi, jet_subleading_met_cos_phi,
+                min_met_cos_phi, max_met_cos_phi, met_sig, sumet_sum_pt_ratio, met_pt_total_ratio]
+
+#
+# Jet Features
+#
+
+@require_jets(2)
+def jet_delta_cos_phi(df):
+    return np.cos(df['PRI_jet_leading_phi'] - df['PRI_jet_subleading_phi'])
+
+def prijet_tau_delta_cos_phi(row):
+    return np.cos(df['PRI_jet_leading_phi'] - df['PRI_tau_phi'])
+
+def prijet_lep_delta_cos_phi(df):
+    return np.cos(df['PRI_jet_leading_phi'] - df['PRI_lep_phi'])
+
+def subjet_lep_delta_cos_phi(df):
+    return np.cos(df['PRI_jet_subleading_phi'] - df['PRI_tau_phi'])
+
+def subjet_lep_delta_cos_phi(df):
+    return np.cos(df['PRI_jet_subleading_phi'] - df['PRI_lep_phi'])
+
+def lep_tau_delta_cos_phi(df):
+    return np.cos(df['PRI_lep_phi'] - df['PRI_tau_phi'])
+
+
+
+jet_features = [jet_delta_cos_phi, prijet_tau_delta_cos_phi, prijet_lep_delta_cos_phi, subjet_lep_delta_cos_phi,
+                subjet_lep_delta_cos_phi, lep_tau_delta_cos_phi]
+
+
+
+# Adding features to a DF
+
+def with_added_features(df):
+
+    df = df.copy()
+
+    print ("Adding momentum features")
+    df = with_momentum_features(df)
+
+    new_features = []
+    new_features.extend(rapidity_features)
+    new_features.extend(z_momentum_features)
+    new_features.extend(transverse_momentum_features)
+    new_features.extend(met_features)
+    new_features.extend(momentum_ratio_features)
+    new_features.extend(mass_energy_features)
+
+    for f in new_features:
+        name = f.__name__
+        print ("Calculating {}".format(name))
+        df[name] = pd.Series(f(df), index=df.index).replace([np.inf, -np.inf], np.nan).fillna(-999)
+
+    return df
